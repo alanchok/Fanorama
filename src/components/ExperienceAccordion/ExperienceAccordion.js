b11e30d70952c14b2ba92f7cf060d2d2ea8c44c1
hamilwood <hamilwood76@gmail.com>
2017-10-28 15:29:43 -0600

    Update ExperienceAccordion.js

--- src/components/ExperienceAccordion/ExperienceAccordion.js
+++ src/components/ExperienceAccordion/ExperienceAccordion.js
@@ -18,12 +18,12 @@ const ExperienceAccordion = () => (
                     title={<div><h3>Fans</h3><span>Preserve the memories and share the bragging rights of their event experience.</span></div>}
                     slug='fans'>
                     <div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.</p>
+                        <p>Enhance the fan experience.</p>
                     </div>
-                    <div className='row points'>ev
+                    <div className='row points'>
                         <div className='col-12 col-sm-6'>
                             <Isvg src={IconFanExperience} className='icon float-left' />
-                            <p>Enhance and preserve memories of the fan experience.</p>
+                            <p>Free digital and printed keepsakes.</p>
                         </div>
                         <div className='col-12 col-sm-6'>
                             <Isvg src={IconPrint} className='icon float-left' />
