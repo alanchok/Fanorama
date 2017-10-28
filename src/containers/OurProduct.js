e6ecb0a29055a332430a24c4227c410d740e8194
hamilwood <hamilwood76@gmail.com>
2017-10-28 17:41:14 -0600

    Update OurProduct.js

--- src/containers/OurProduct.js
+++ src/containers/OurProduct.js
@@ -45,8 +45,8 @@ class OurProduct extends Component {
                 </div>
                 <SimpleSlider children={[
                     <div className='hero-copy' key='about-us'>
-                        <h3>Stand Out From The Crowd</h3>
-                        <p>Our mission is to capture every moment that matters to fans during the big event. Consectetur adipiscing elit. Vivamus molestie dui vitae velit facilisis convallis. Sed et maximus purus, vel posuere nunc. In tristique vel lacus ac dapibus. Etiam sed auctor neque. Curabitur sollicitudin tellus sed ante dapibus, vitae tincidunt eros ultrices.</p>
+                        <h3>Obsessed with the fan experience</h3>
+                        <p>Be your fans biggest Fan. Use the Fanorama platform to give people what they really want, bragging rights and a piece of the action.</p>
                     </div>,
                     <div className='hero-copy' key='about-us-2'>
                         <h3>Stand Out From The Crowd</h3>
