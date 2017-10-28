0e5a0582ed9fb26dc2f620e04af975b2f31ee549
hamilwood <hamilwood76@gmail.com>
2017-10-28 16:01:40 -0600

    Update ExperienceAccordion.js

--- src/components/ExperienceAccordion/ExperienceAccordion.js
+++ src/components/ExperienceAccordion/ExperienceAccordion.js
@@ -47,7 +47,9 @@ const ExperienceAccordion = () => (
                     </div>
                 </AccordionItem>
                 <AccordionItem
-                    title={<div><h3>Teams/Venues</h3><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.</span></div>}
+                    title={<div><h3>Teams, Leagues, and Venues</h3><span>Forge stronger ties with fans, teams and brands and increase exposure of your events.
+ 
+</span></div>}
                     slug='teams'>
                     <div>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.</p>
